429eee7e28e1ba2e9c5a7346e98ba4552de8db4c
Matilda-bit <polinaovras@gmail.com>
2019-10-26 18:10:41 +0300

    todo item some new style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
+import todosData from "./todosData";
+import TodoItem from "./TodoItem";
 
 class App extends React.Component {
     constructor (){
         super()
         this.state = {
-            isLoggedIn : false}
-        this.handleClick = this.handleClick.bind(this)
+            todos : todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleClick() {
+    handleChange(id) {
+        console.log("Changed",id)
         this.setState(prevState => {
-            return {
-                isLoggedIn: !prevState.isLoggedIn
-            }
-         })
+            const updateTodos = prevState.todos.map( todo => {
+                if(todo.id===id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+        return {
+            todos: updateTodos
+        }
+        })
     }
     render() {
-        let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
-        let displayText = this.state.isLoggedIn ? "YOU LOG IN" : "YOU LOG OUT"
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item ={item}
+        handleChange={this.handleChange}/>)
+
         return (
             <div>
-                <button onClick={this.handleClick}>{buttonText}</button>
-                <h1>{displayText}</h1>
-
+                {todoItems}
             </div>
         )
     }
