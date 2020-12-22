9512dcba159fe03f53eebe51be3f9cbf1a18b1d8
Matilda-bit <polinaovras@gmail.com>
2020-12-22 20:13:28 +0200

    styling

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,13 @@ function Header(){
 
         <header>
             <img
-                src="https://img.tsn.ua/cached/1533893587/tsn-5158ca2e1423d82bb1a288ac9a4ee131/thumbs/990x540/9b/8d/c3c8e52e7d1d92ca323a06ad4fb08d9b.jpg"
+                //src="https://img.tsn.ua/cached/1533893587/tsn-5158ca2e1423d82bb1a288ac9a4ee131/thumbs/990x540/9b/8d/c3c8e52e7d1d92ca323a06ad4fb08d9b.jpg"
+                src="https://img.icons8.com/clouds/2x/angry-face-meme.png"
                 alt="Problem?"
+                // className="logo"
             />
 
-            <p> >> Meme Generator  </p>
+            <p>Meme Generator</p>
         </header>
 
     )
