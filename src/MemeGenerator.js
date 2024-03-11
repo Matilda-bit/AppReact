d659866917380a1c1d01db618a1070aad320ba27
Matilda-bit <polinaovras@gmail.com>
2024-03-11 23:23:10 +0200

    reformat to multiply lines, add a text line

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./utils/MemesLink";
-import meme from "./utils/MemesLink";
-// import AlignLeftIcon from './assets/icons/textAlign/align-left.png';
-// import AlignCenterIcon from './assets/icons/textAlign/format.png';
-// import AlignRightIcon from './assets/icons/textAlign/align-right.png';
-// import FontSize from './assets/icons/fontSize/font-size.png';
-// import ReduceSize from './assets/icons/fontSize/down.png';
-// import IncreaseSize from './assets/icons/fontSize/up.png';
 import BtnIcon from './assets/icons/btn/pokeball.png';
+import Settings from './Settings'; // assuming you have created a separate Settings component
 
-import Settings from './Settings';
+import AddIcon from './assets/icons/btn/add.png';
 
 const MemeGenerator = () => {
-    const [topText, setTopText] = useState("");
-    const [bottomText, setBottomText] = useState("");
-    const [topColor, setTopColor] = useState("color-white");
-    const [bottomColor, setBottomColor] = useState("color-white");
-    const [textAlignTop, setTextAlignTop]= useState(' text-align-center ');
-    const [textAlignBottom, setTextAlignBottom] = useState(' text-align-center ');
-    const [fontSizeTop, setFontSizeTop] = useState(10);
-    const [fontSizeBottom, setFontSizeBottom] = useState(10);
+    const [lines, setLines] = useState([
+        {
+            title: "Top Text",
+            text: "",
+            color: "color-white",
+            textAlign: 'text-align-center',
+            fontSize: 10
+        },
+        {
+            title: "Bottom Text",
+            text: "",
+            color: "color-white",
+            textAlign: 'text-align-center',
+            fontSize: 10
+        }
+    ]);
     const [hideSettings, setHideSettings] = useState(true);
-    const reduce = fontSizeTop > 6;
-    const increase = fontSizeTop < 20;
     const [allMemeImgs, setAllMemeImgs] = useState([]);
     const [item, setItem] = useState({
         id: "61579",
@@ -34,9 +34,6 @@ const MemeGenerator = () => {
         data: [],
     });
 
-    const [topLimit, setTopLimit] = useState(0);
-    const [bottomLimit, setBottomLimit] = useState(0);
-
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
@@ -47,58 +44,70 @@ const MemeGenerator = () => {
             });
     }, []);
 
-    const handleChange = (event) => {
-        console.log(item.data);
-        console.log(meme);
-        console.log(allMemeImgs);
-        //allMemeImgs
-        
+
+
+
+    const handleChange = (event, index) => {
         const { name, value } = event.target;
-        if (name === "topText") {
-            let el1 = document.getElementById("Top Text");
-            if(el1){
-                setTopLimit(el1.offsetHeight);
-            }
-            if(topLimit < (item.height/2) || topText > value) {
-                setTopText(value);
-            }
-        } else if (name === "bottomText") {
-                let el1 = document.getElementById("Bottom Text");
-                if(el1){
-                    setBottomLimit(el1.offsetHeight);
+        setLines(prevLines => {
+            return prevLines.map((line, i) => {
+                if (i === index) {
+                    return { ...line, [name]: value };
                 }
-                if(bottomLimit < (item.height/2) || bottomText > value) {
-                    setBottomText(value);
-                }
-        }
-    };
-
-    //checkboxChange
-
-    const checkboxChange = (event) => {
-        
-        const {name } = event.target;
-        if (name === "checkbox") {
-            setTextAlignBottom(textAlignTop);
-            setBottomColor(topColor);
-            setHideSettings(!hideSettings);
-        } 
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const randNum = Math.floor(Math.random() * allMemeImgs.length);
-        const randMeme = allMemeImgs[randNum];
-        setItem({
-            id: randMeme.id,
-            box_count: randMeme.box_count,
-            height: randMeme.height,
-            width: randMeme.width,
-            name: randMeme.name,
-            img: randMeme.url,
-            data: randMeme
+                return line;
+            });
         });
     };
+
+
+
+        //checkboxChange
+
+        const checkboxChange = (event) => {
+            const { name } = event.target;
+            if (name === "checkbox") {
+                setLines(prevLines => {
+                    return prevLines.map((line, index) => {
+                        if (index !== 0) {
+                            return {
+                                ...line,
+                                textAlign: lines[0].textAlign,
+                                color: lines[0].color
+                            };
+                        }
+                        return line;
+                    });
+                });
+                setHideSettings(!hideSettings);
+            }
+        };
+        
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            const randNum = Math.floor(Math.random() * allMemeImgs.length);
+            const randMeme = allMemeImgs[randNum];
+            setItem({
+                id: randMeme.id,
+                box_count: randMeme.box_count,
+                height: randMeme.height,
+                width: randMeme.width,
+                name: randMeme.name,
+                img: randMeme.url,
+                data: randMeme
+            });
+        };
+
+        const addLine = () => {
+            setLines(prevLines => [
+                ...prevLines,
+                {
+                    text: "",
+                    color: "color-white",
+                    textAlign: 'text-align-center',
+                    fontSize: 10
+                }
+            ]);
+        };
 
     return (
         <div>
@@ -110,85 +119,69 @@ const MemeGenerator = () => {
             </div>
 
             <form className="meme-form" onSubmit={handleSubmit}>
-                <div className="meme-input">
-                    <label className="text-bold comic-font">Top Text</label>
-                    <Settings
-                        setColor={setTopColor}
-                        setTextAlign={setTextAlignTop}
-                        setFontSize={setFontSizeTop}
-                        setColorTotal={setBottomColor}
-                        setTextAlignTotal={setTextAlignBottom}
-                        setFontSizeTotal={setFontSizeBottom}
-                        color={topColor}
-                        textAlign={textAlignTop}
-                        fontSize={fontSizeTop}
-                        reduce={reduce}
-                        increase={increase}
-                        hideSettings={hideSettings}
-                    />
-
-                    <textarea
-                        type="text"
-                        name="topText"
-                        className="margin-10 input-pretender"
-                        placeholder="Top Text"
-                        value={topText}
-                        onChange={handleChange}
-                    />
-
-                    <label className="pt-5 text-bold comic-font">Bottom Text</label>
-                    <div >
-                        <label className="same-settings">Same Settings
-                            <input className="checkbox" name="checkbox" type="checkbox" checked={hideSettings} onChange={checkboxChange}/>
-                            <span className="checkmark"></span>
-                        </label>
-                    </div>
-
-                    {!hideSettings ? (
-                        <Settings
-                        setColor={setBottomColor}
-                        setTextAlign={setTextAlignBottom}
-                        setFontSize={setFontSizeBottom}
-                        color={bottomColor}
-                        textAlign={textAlignBottom}
-                        fontSize={fontSizeBottom}
-                        reduce={reduce}
-                        increase={increase}
-                        hideSettings={hideSettings}
-                    />)
-
-                     : null}
-
-                    <textarea
-                        type="text"
-                        name="bottomText"
-                        className="margin-10 input-pretender"
-                        placeholder="Bottom Text"
-                        value={bottomText}
-                        onChange={handleChange}
-                    />
-
-                    <button className="">SUBMIT</button>
-                </div>
-
-                <div className="display-flex-col">
-                    <div className="center settings-title grey">
-                        <h2 className="center">"{item.name}"</h2>
-                    </div>
-                    <div className="meme limit">
-                        <img src={item.img} alt={item.name} />
-                        <h2 id="Top Text" className={"top " + topColor + " " + textAlignTop + " font-size-" + fontSizeTop} style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{topText}</h2>
-                        <h2 id="Bottom Text" className={"bottom " + bottomColor + " " + textAlignBottom + " font-size-" + fontSizeBottom} style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{bottomText}</h2>
+            <div className="meme-input">
+                {lines.map((line, index) => (
+                    <div>
+                    <div key={index} className="meme-input-item">
+                        <label className="text-bold comic-font">{index === 0 ? "Text " + (index+1) : "Text " + (index+1)}</label>
+                        {/* {!hideSettings &&  */}
+                        <Settings 
+                            index={index}
+                            template={line[0]}
+                            setColor={lines[index].color}
+                            setTextAlign={lines[index].textAlign}
+                            setFontSize={lines[index].fontSize}
+                            // reduce={reduce}
+                            // increase={increase}
+                            hideSettings={hideSettings}
+                        />
+                        {/* } */}
+                        <textarea
+                            type="text"
+                            name="text"
+                            className="margin-10 input-pretender"
+                            placeholder={index === 0 ? "Top Text" : "Bottom Text"}
+                            value={line.text}
+                            onChange={(event) => handleChange(event, index)}
+                        />         
                     </div>
                     
-                </div>
+                    {(index === 0) ? 
+                        (<div >
+                            <label className="same-settings">Same settings to all lines
+                                <input className="checkbox" name="checkbox" type="checkbox" checked={hideSettings} onChange={checkboxChange}/>
+                                <span className="checkmark"></span>
+                            </label>
+                        </div>) : null}
+                        </div>
+                    
+                ))}
 
+                <div className="meme-input-item">
+                <button className="meme-input" type="button" onClick={addLine}> <img src={AddIcon} alt="buttonpng" border="0" width={35} height={35} /> 
+                    Add Line</button>
                 
-            </form>
+                    <button className="meme-input-item">SUBMIT</button>
+                </div>
+            </div>
+            <div className="display-flex-col">
+                <div className="center settings-title grey">
+                    <h2 className="center">"{item.name}"</h2>
+                </div>
+                <div className="meme limit">
+                    <img src={item.img} alt={item.name} />
+                    {lines.map((line, index) => (
+                        <h2 key={index} className={`line- ${line.color} ${line.textAlign} font-size-${line.fontSize}`} style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{line.text}</h2>
+                    ))}
+                </div>
+            </div>
 
             <div className="center row">
                 <label > original size {item.width} x {item.height}</label>
             </div>
+            </form>
+
+            
             
         </div>
     );
--- src/Settings.js
+++ src/Settings.js
@@ -6,7 +6,13 @@ import FontSize from './assets/icons/fontSize/font-size.png';
 import ReduceSize from './assets/icons/fontSize/down.png';
 import IncreaseSize from './assets/icons/fontSize/up.png';
 
-const Settings = ({ setColor, setTextAlign, setFontSize, setColorTotal, setTextAlignTotal, setFontSizeTotal, color, textAlign, fontSize, reduce, increase, hideSettings }) => {
+const Settings = ({index, template, setColor, setTextAlign, setFontSize, /*reduce, increase, */ hideSettings }) => {
+    const tem = index === 0; 
+    let reduce = setFontSize > 6;
+    let increase = setFontSize < 20;
+    if(!tem && hideSettings) {
+        return null;
+    }
     return (
         <div className="settings">
             <div className="input-settings-color">
@@ -15,12 +21,12 @@ const Settings = ({ setColor, setTextAlign, setFontSize, setColorTotal, setTextA
                     <div
                         className="color-option"
                         style={{ backgroundColor: "black" }}
-                        onClick={() => { setColor('color-black'); hideSettings && setColorTotal('color-black'); }}
+                        onClick={() => { setColor(hideSettings ? template.color : 'color-black'); }}
                     />
                     <div
                         className="color-option"
                         style={{ backgroundColor: "white" }}
-                        onClick={() => { setColor('color-white'); hideSettings && setColorTotal('color-white'); }}
+                        onClick={() => { setColor('color-white'); hideSettings && setColor('color-white'); }}
                     />
                 </div>
             </div>
@@ -28,13 +34,13 @@ const Settings = ({ setColor, setTextAlign, setFontSize, setColorTotal, setTextA
             <div className="input-settings-color">
                 <label className="settings-title">Text Align</label>
                 <div className="display-flex">
-                    <div className="align-option" onClick={() => { setTextAlign(' text-align-left '); hideSettings && setTextAlignTotal(' text-align-left '); }}>
+                    <div className="align-option" onClick={() => { setTextAlign(' text-align-left '); hideSettings && setTextAlign(' text-align-left '); }}>
                         <img src={AlignLeftIcon} alt="Left Align" width="25" height="15" />
                     </div>
-                    <div className="align-option" onClick={() => { setTextAlign(' text-align-center '); hideSettings && setTextAlignTotal(' text-align-center '); }}>
+                    <div className="align-option" onClick={() => { setTextAlign(' text-align-center '); hideSettings && setTextAlign(' text-align-center '); }}>
                         <img src={AlignCenterIcon} alt="Center Align" width="25" height="15" />
                     </div>
-                    <div className="align-option" onClick={() => { setTextAlign(' text-align-right '); hideSettings && setTextAlignTotal(' text-align-right '); }}>
+                    <div className="align-option" onClick={() => { setTextAlign(' text-align-right '); hideSettings && setTextAlign(' text-align-right '); }}>
                         <img src={AlignRightIcon} alt="Right Align" width="25" height="15" />
                     </div>
                 </div>
@@ -43,13 +49,13 @@ const Settings = ({ setColor, setTextAlign, setFontSize, setColorTotal, setTextA
             <div className="input-settings-color">
                 <label className="settings-title">Font Size</label>
                 <div className="display-flex">
-                    <fieldset className="font-size-option" onClick={() => reduce ? setFontSize(fontSize - 1) : null}>
+                    <fieldset className="font-size-option" onClick={() => reduce ? setFontSize(... - 1) : null}>
                         <img src={ReduceSize} alt="Font Size" width="15" height="15" />
                     </fieldset>
                     <div className="font-size-option no-cursor">
                         <img src={FontSize} alt="Font Size" width="25" height="25" />
                     </div>
-                    <fieldset className="font-size-option" onClick={() => increase ? setFontSize(fontSize + 1) : null}>
+                    <fieldset className="font-size-option" onClick={() => increase ? setFontSize(... + 1) : null}>
                         <img src={IncreaseSize} alt="Font Size" width="15" height="15" />
                     </fieldset>
                 </div>
