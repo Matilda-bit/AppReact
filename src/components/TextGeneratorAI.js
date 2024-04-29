07afad295b7ad5dc1eb591f6758cb202460834f2
Matilda-bit <polinaovras@gmail.com>
2024-04-29 15:55:34 +0300

    edited content text

--- src/components/TextGeneratorAI.js
+++ src/components/TextGeneratorAI.js
@@ -5,21 +5,29 @@ import apiKey from '../utils/ai/apiKey';
 const ai = new OpenAIApi(new Configuration({ apiKey }));
 
 const topics = [
-              'Everyday Life', 
-              'Work', 
-              'Ralationships', 
-              'Technology', 
-              'Food', 
-              'Travel', 
-              'Animals',
-              'Sports',
-              'School',
-              'Health',
-              'Fashion'
-             ]
+  'Everyday Life', 
+  'Work', 
+  'Ralationships', 
+  'Technology', 
+  'Food', 
+  'Travel', 
+  'Animals',
+  'Sports',
+  'School',
+  'Health',
+  'Fashion',
+  'Money',
+  'Hobbies',
+  'Pop Culture',
+  'Self-Deprecating Humor'
+];
+
+const topicsString = topics.join(', ');
 //babel
-function TextGeneratorAI() {
+function TextGeneratorAI({ memeTitle, memeDescription, box_count }) {
   const [generatedText, setGeneratedText] = useState('');
+
+  const description = memeDescription ? `(${memeTitle} description: ${memeDescription})` : "";
 
   useEffect(() => {
     async function generateText () {
@@ -34,7 +42,14 @@ function TextGeneratorAI() {
       //const response = await ai.createChatCompletion({
       const response = () => { ai.createChatCompletion({
         model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: 'Hello, write a script for a video about neural networks.' }]
+        messages: [{ 
+          role: 'user', 
+          content: 
+          `Hello, write a joke for each topic for the meme "${memeTitle}".
+          ${description} 
+          topics: ${topicsString}.
+          count of lines: ${box_count} 
+          ` }]
       })};
       const content = response.data.choices[0].message.content;
       setGeneratedText(content);
