a647df2dde275af71738957a405c42384ecebb3c
Matilda-bit <polinaovras@gmail.com>
2024-07-11 17:04:04 +0300

    fetch

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -10,18 +10,22 @@ export async function fetchAvailableMemes() {
   }
   
   export async function fetchUserMemes() {
+    console.log("fetchUserMemes...");
     const response = await fetch('http://localhost:3000/user-memes');
     const resData = await response.json();
+    console.log("OK");
+    console.log(resData);
+  
   
     if (!response.ok) {
       throw new Error('Failed to fetch user memes');
     }
   
-    return resData.memes;
+    return resData;
   }
   
   export async function updateUserMemes(memes) {
-    const response = await fetch('http://localhost:3000/user-memes', {
+    const response = await fetch('https://localhost:3000/user-memes', {
       method: 'PUT',
       body: JSON.stringify({ memes }),
       headers: {
@@ -36,4 +40,18 @@ export async function fetchAvailableMemes() {
     }
   
     return resData.message;
+  }
+
+  export async  function fetchMemeTemplates() {
+    console.log("fetchMemeTemplates...");
+    const response = await fetch("https://api.imgflip.com/get_memes");
+    const resData = await response.json();
+  
+    // if (!response.ok) {
+    //   throw new Error('Failed to fetch places');
+    // }
+
+    console.log(resData);
+  
+    return resData;
   }
