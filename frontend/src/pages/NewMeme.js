60d2174a7a5853e4b0f2636d1e46266821524c99
Matilda-bit <polinaovras@gmail.com>
2024-07-11 16:59:23 +0300

    user memes

--- frontend/src/pages/NewMeme.js
+++ frontend/src/pages/NewMeme.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import MemeForm from '../components/MemeForm';
+// import AvailableMemeTemplates from '../components/AvailableMemeTemplates';
 
 function NewMemePage() {
-  console.log("Im here!");
-  return <MemeForm method="post" />;
+  console.log("NewMemePage...");
+
+  return (
+  <>
+    <MemeForm method="post" />
+  </>);
 }
 
 export default NewMemePage;
---
+++ frontend/src/pages/UserMemes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Suspense } from 'react';
+import { useLoaderData, defer, Await } from 'react-router-dom';
+
+import MemesList from '../components/MemesList';
+
+import { fetchUserMemes } from '../http.js';
+
+function UserMemesPage() {
+  const { userMemes } = useLoaderData();
+
+  console.log("UserMemesPage...");
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={userMemes}>
+        {(loadedMemes) => <MemesList memes={loadedMemes} />}
+      </Await>
+    </Suspense>
+  );
+}
+
+export default UserMemesPage;
+
+export function loader() {
+  return defer({
+    memes: fetchUserMemes(),
+  });
+}
