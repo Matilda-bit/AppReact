b5a501a43cd9de4a66994dfd71ebdfda0e11874b
Matilda-bit <polinaovras@gmail.com>
2025-03-04 20:14:29 +0200

    Home page and Header design

--- frontend/src/components/home/HomeContent.js
+++ frontend/src/components/home/HomeContent.js
@@ -1,11 +1,8 @@
 import classes from "./HomeContent.module.css";
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-
 function HomeContent() {
-
   const [showGif, setShowGif] = useState(true);
 
   useEffect(() => {
@@ -13,12 +10,11 @@ function HomeContent() {
     const timer = setTimeout(() => {
       setShowGif(false);
     }, duration);
-
     return () => clearTimeout(timer);
   }, []);
-  console.log('Welcome to the Home page \nHomeContent rendered...')
+
   return (
-    <>
+    <div className={classes.home}>
       <p className={classes.description}>
         Browse our amazing <Link to="/user-memes" className={classes.link}>Memes</Link> or create a <Link to="/memes/new" className={classes.link}>New One</Link>!
         <br />A funny moments can make your day...
@@ -27,33 +23,26 @@ function HomeContent() {
         <br />Create your own unique brand of humor and brighten someone else's day in minutes!
         <br />
       </p>
-      {showGif ? (
-        <img
-          src="https://media.giphy.com/media/MNmyTin5qt5LSXirxd/giphy.gif"
-          alt="Funny GIF"
-          className="gif"
-        />
-      ) : (
-        <img
-          src="https://media.giphy.com/media/MNmyTin5qt5LSXirxd/giphy_s.gif"
-          alt="Static GIF Frame"
-          className="gif"
-        />
-      )}
-      <p>
-        Imagine this: you’re scrolling through your feed after a long day,
-        chuckling at the memes that friends and strangers have shared.
-        Suddenly, you remember that hilarious moment from earlier—the one that made you laugh out loud.
-        Maybe it was your pet’s unexpected antics, a funny twist in a conversation,
-        or even a clever thought you had. You think, this would make a perfect meme.
-      </p>
+      <div className={classes.gifContainer}>
+        {showGif ? (
+          <img src="https://media.giphy.com/media/MNmyTin5qt5LSXirxd/giphy.gif" alt="Funny GIF" className={classes.gif} />
+        ) : (
+          <img src="https://media.giphy.com/media/MNmyTin5qt5LSXirxd/giphy_s.gif" alt="Static GIF Frame" className={classes.gif} />
+        )}
+      </div>
 
-      <img
-        src="https://ww2.kqed.org/app/uploads/sites/38/2019/03/tomemeornot3.jpg"
-        alt="to meme or not to meme" width={750} height={335}
-        className="" />
+        <p>
+          Imagine this: you’re scrolling through your feed after a long day,
+          chuckling at the memes that friends and strangers have shared.
+          Suddenly, you remember that hilarious moment from earlier—the one that made you laugh out loud.
+          Maybe it was your pet’s unexpected antics, a funny twist in a conversation,
+          or even a clever thought you had. You think, this would make a perfect meme.
+        </p>
 
-      <p>
+
+      <div className={classes.contentRow}>
+        <img src="https://ww2.kqed.org/app/uploads/sites/38/2019/03/tomemeornot3.jpg" alt="To meme or not to meme" className={classes.imageLeft} />
+        <p>
         With MemeByte, you don’t have to keep that laugh to yourself.
         Here, you can capture those moments, add your unique twist,
         and turn them into something shareable. In just a few clicks,
@@ -61,12 +50,11 @@ function HomeContent() {
         And who knows? Maybe your creation will be the one that goes viral,
         spreading smiles across the internet!
       </p>
+      </div>
 
-      <img
-        src="https://napoleoncat.com/wp-content/uploads/2022/05/social-media-memes-toy-story-meme.jpg"
-        alt="memes memes is everywhere" width={735} height={500}
-        className="" />
-
+      <div className={classes.contentRowReverse}>
+      <img src="https://napoleoncat.com/wp-content/uploads/2022/05/social-media-memes-toy-story-meme.jpg" alt="Memes are everywhere" className={classes.imageRight} />
+      
       <p>
         So, why not give it a try?
         Dive into the MemeByte meme generator, unleash your creativity,
@@ -74,11 +62,15 @@ function HomeContent() {
         Because let’s face it—the internet can always use more laughter,
         and you’ve got just the wit to bring it.
       </p>
+      </div>
+
+      
       <Link to="/memes/new" className={classes.button}>Create Meme</Link>
       <p className={classes.description}>Browse our memes to laugh or get inspired and find a humorous muse to create your own meme!</p>
       <Link to="/memes" className={classes.button}>Memes</Link>
       <hr className="solid"></hr>
       <p className={classes["coming-soon"]}>Will be added soon:</p>
+      
       <ul className={classes.list}>
         <li><strong>Advertising</strong>:</li>
         <p className={classes.updates}> - Banners and display advertising:
@@ -95,7 +87,7 @@ function HomeContent() {
           <br />Create an online store selling meme-related goods such as: T-shirts, mugs and other souvenirs.</p>
         <hr className="solid"></hr>
       </ul>
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/old/Header.js
+++ frontend/src/components/old/Header.js
@@ -5,10 +5,11 @@ function Header(){
     return(
         <header className={classes.staticHeader}>
             <img
-                src="https://img.icons8.com/clouds/2x/angry-face-meme.png"
+                src="https://media4.giphy.com/media/tR1ZZeJXR9RUDvaFVP/giphy.gif?cid=6c09b952id1ipxo7y46s143tcbc684hfpx91t9b4f17fx282&ep=v1_stickers_search&rid=giphy.gif&ct=s"
                 alt="Problem?"
             />
             <p>Meme Byte</p>
+            <div class={classes["neon-line"]}></div>
         </header>
     );
 }
